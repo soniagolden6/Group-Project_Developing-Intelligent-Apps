a6d4d21173b91b0d0b9ce8b937b15195b058c920
Justin Collier <justin.collier@dcmail.ca>
2024-08-03 17:34:02 -0400

    DEBUG: Frontend /predict page
    + Re-exported the .pkl file for our tokenizer
    + Changed project architecture/file formatting
    ~ This also serves as a backup for the application before I debug the /predict page and begin attempting to fix the current value error I have been getting

--- backend/app.py
+++ backend/app.py
@@ -8,8 +8,8 @@ import pickle
 app = Flask(__name__)
 
 # Load the sentiment analysis model and tokenizer
-model = load_model('models/sentiment_model.h5')
-with open('models/tokenizer.pkl', 'rb') as handle:
+model = load_model('../models/sentiment_model.h5')
+with open('../models/tokenizer.pkl', 'rb') as handle:
     tokenizer = pickle.load(handle)
 
 max_sequence_length = 100  # same as during model training
@@ -22,6 +22,9 @@ def home():
 def predict():
     if request.method == 'POST':
         text = request.form['text']
+        if not text:
+            return render_template('index.html', error='Please enter text for prediction.')
+        
         sequences = tokenizer.texts_to_sequences([text])
         padded_sequences = pad_sequences(sequences, maxlen=max_sequence_length)
         prediction = model.predict(padded_sequences)[0][0]
