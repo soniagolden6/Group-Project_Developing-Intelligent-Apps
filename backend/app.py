fd3cfc40f6f43fd47f1bca0690f41e874e53bdbf
Jashan Gill <jashanpreetKaur.Gill@dcmail.ca>
2024-08-07 10:42:31 -0400

    Enabled CORS in app.py to resolve cross-origin issues

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, render_template, jsonify
+from flask import Flask, request, jsonify
+from flask_cors import CORS  # Import CORS
 import tensorflow as tf
 from tensorflow.keras.models import load_model
 import numpy as np
@@ -6,6 +7,7 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 import pickle
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
 
 # Load the sentiment analysis model and tokenizer
 model = load_model('../models/sentiment_model.h5')
@@ -16,22 +18,22 @@ max_sequence_length = 200  # Updated to 200
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return "Sentiment Analysis API"
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    if request.method == 'POST':
-        text = request.form['text']
-        if not text:
-            return render_template('index.html', error='Please enter text for prediction.')
-        
-        sequences = tokenizer.texts_to_sequences([text])
-        padded_sequences = pad_sequences(sequences, maxlen=max_sequence_length)
-        prediction = model.predict(padded_sequences)[0][0]
+    data = request.get_json()
+    text = data['text']
+    if not text:
+        return jsonify({'error': 'Please enter text for prediction.'}), 400
 
-        sentiment = 'Positive' if prediction <= 0.5 else 'Negative'  # Adjusting threshold to 0.5
+    sequences = tokenizer.texts_to_sequences([text])
+    padded_sequences = pad_sequences(sequences, maxlen=max_sequence_length)
+    prediction = model.predict(padded_sequences)[0][0]
 
-        return render_template('index.html', text=text, prediction=sentiment)
+    sentiment = 'Positive' if prediction >= 0.5 else 'Negative'  # Adjusting threshold to 0.5
+
+    return jsonify({'text': text, 'prediction': sentiment})
 
 if __name__ == '__main__':
     app.run(debug=True)
