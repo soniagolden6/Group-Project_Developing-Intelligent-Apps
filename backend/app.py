5b54bb2de6a2f7225328f6391b3fce98803a96e6
Jashan Gill <jashanpreetKaur.Gill@dcmail.ca>
2024-08-09 16:30:12 -0400

    updated some files: app, sentiment_model, tokenizer, train_model

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-from flask_cors import CORS  # Import CORS
+from flask_cors import CORS
 import tensorflow as tf
 from tensorflow.keras.models import load_model
 import numpy as np
@@ -7,14 +7,14 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 import pickle
 
 app = Flask(__name__)
-CORS(app)  # Enable CORS for all routes
+CORS(app)
 
 # Load the sentiment analysis model and tokenizer
 model = load_model('../models/sentiment_model.h5')
 with open('../models/tokenizer.pkl', 'rb') as handle:
     tokenizer = pickle.load(handle)
 
-max_sequence_length = 200  # Updated to 200
+max_sequence_length = 200
 
 @app.route('/')
 def home():
@@ -31,7 +31,8 @@ def predict():
     padded_sequences = pad_sequences(sequences, maxlen=max_sequence_length)
     prediction = model.predict(padded_sequences)[0][0]
 
-    sentiment = 'Positive' if prediction >= 0.5 else 'Negative'  # Adjusting threshold to 0.5
+    # Adjusting threshold to 0.5, assuming lower score means negative sentiment
+    sentiment = 'Positive' if prediction > 0.5 else 'Negative'
 
     return jsonify({'text': text, 'prediction': sentiment})
 
